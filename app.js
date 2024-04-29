7b2aeee70492e760750558bf8af5c4ae7ac43987
Cameron Taylor <cameron.taylor@microsoft.com>
2024-04-29 16:28:13 -0700

    2 commits

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello GitOps and Argo CD! (I only made 1 commit to change this message)'))
+app.get('/', (req, res) => res.send('Hello GitOps and Argo CD! (I only made 2 commits to change this message)'))
 app.listen(3000, () => console.log('Server ready'))
